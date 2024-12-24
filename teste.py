d471b7235cd0aad6b4ec7c0b543b939606ef4fbe
yuri <yuri03ysl@gmail.com>
2024-12-24 10:00:40 -0300

    updating

--- teste.py
+++ teste.py
@@ -1,2 +1,15 @@
-for i in range(10):
-    print("Don't give up!")
+import gi
+
+
+gi.require_version("Gtk", "3.0")
+
+from gi.repository import Gtk
+
+
+win = Gtk.Window()
+
+win.connect("destroy", Gtk.main_quit)
+
+win.show_all()
+
+Gtk.main()
