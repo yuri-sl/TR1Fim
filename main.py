850a56a6401eb008400a81b53064cb1054486c65
Luisgds <luisgdscat10@gmail.com>
2025-01-02 16:28:24 -0300

    menu suspenso e base de graficos

--- main.py
+++ main.py
@@ -8,15 +8,21 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_gtk3agg import FigureCanvasGTK3Agg as FigureCanvas
 from matplotlib.figure import Figure
 
-from apper_graph_nrz import Apper_graph_nrz
 from caixa_texto import EntryWindow
-from graficos import Apper_graph
+from menu_suspenso import MenuSuspenso
+
+from graficos.apper_graph_nrz import Apper_graph_nrz
+from graficos.apper_graph_bipolar import Apper_graph_bipolar
+from graficos.apper_graph_manchester import Apper_graph_manchester
+from graficos.apper_graph_ask import Apper_graph_ask
+from graficos.apper_graph_fsk import Apper_graph_fsk
+from graficos.apper_graph_8qam import Apper_graph_8qam
+
 
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Simulador transmissores e receptores")
         self.set_border_width(10)
-
         # Create a grid layout
         self.grid = Gtk.Grid()
         self.add(self.grid)
@@ -25,34 +31,25 @@ class MainWindow(Gtk.Window):
         self.entry_window = EntryWindow()
         self.grid.attach(self.entry_window, 0, 0, 1, 1)
 
-        # Button to trigger graph display
-        button_add_graph = Gtk.Button(label="Adicione o gráfico aqui!/python bin")
-        button_add_graph.connect("clicked", self.apper_new_graph)
-        self.grid.attach(button_add_graph, 0, 1, 1, 1)
-
         # Button para aparecer a palavra em binario
         button_add_graph = Gtk.Button(label="Palavra em bin")
         button_add_graph.connect("clicked", self.apper_WordToBin)
         self.grid.attach(button_add_graph, 1, 0, 1, 1)
-
-        # Atualiza Button
-        button_new_graph = Gtk.Button(label="grafico em nrz")
-        button_new_graph.connect("clicked", self.graph_NRZ)
-        self.grid.attach(button_new_graph, 1, 1, 1, 1)
 
         # Primary Label
         self.label = Gtk.Label(label="Irá aparecer o texto em binario aqui")
         self.label.set_halign(Gtk.Align.END)
         self.grid.attach(self.label, 0, 2, 2, 1)
 
-        # Secondary Label (to be dynamically added)
-        self.labelTeste = Gtk.Label(label=f"PYTHON!!!")
-        self.labelTeste.set_halign(Gtk.Align.CENTER)
+
+        # Criação da instância da menu_suspenso (do arquivo menu_suspenso.py)
+        self.menu = MenuSuspenso(self)
+        self.grid.attach(self.menu, 4, 0, 1, 1)
+
 
         self.label_added = False  # Control flag for adding label
-
         self.maximize()
-
+        self.grid.show_all()         
 
     #Aparece a palavra em binário
     def apper_WordToBin(self, widget):
@@ -63,8 +60,7 @@ class MainWindow(Gtk.Window):
             binarios.append(valor_binario)  # Adiciona o valor binário à lista
         self.label.set_text(f"{binarios} = {palavra} em bin")
 
-
-    def graph_NRZ(self, widget):
+    def graph_NRZ(self):
         palavra = self.entry_window.get_text()
         binarios = [] 
         for char in palavra:
@@ -83,12 +79,26 @@ class MainWindow(Gtk.Window):
         # Adiciona o gráfico ao grid na posição especificada
         self.grid.attach(canvas, 0, 4, 4, 4)  # Coluna 4, linha 4, ocupando 2x2 células
         self.show_all()
-
-
-    def apper_new_graph(self, widget):
+    def graph_Bipolar(self):
         a = [1, 2, 3, 4]
         b = [0, 4, 9, 16]
-        Apper_graph(a,b)
+        Apper_graph_bipolar(a,b)
+    def graph_Manchester(self):
+        a = [1, 2, 3, 4]
+        b = [0, 4, 9, 16]
+        Apper_graph_manchester(a,b)
+    def graph_ASK(self):
+        a = [1, 2, 3, 4]
+        b = [0, 4, 9, 16]
+        Apper_graph_ask(a,b)
+    def graph_FSK(self):
+        a = [1, 2, 3, 4]
+        b = [0, 4, 9, 16]
+        Apper_graph_fsk(a,b)
+    def graph_8QAM(self):
+        a = [1, 2, 3, 4]
+        b = [0, 4, 9, 16]
+        Apper_graph_8qam(a,b)
     
 
 window = MainWindow()
